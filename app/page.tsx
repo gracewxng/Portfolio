503dd0c97aa75c60416e6903a589d5655d18d13d
Grace <wanggrace730@gmail.com>
2024-09-17 14:14:54 -0700

    faster fade in

--- app/page.tsx
+++ app/page.tsx
@@ -10,30 +10,30 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-pink-100 via-pink-200 to-pink-300">
-      <nav className="my-16 animate-fade-in duration-500">
+      <nav className="my-16 animate-fade-in duration-200">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-lg md:text-xl duration-500 text-gray-400 hover:text-gray-300"
+              className="text-lg md:text-xl duration-200 text-gray-400 hover:text-gray-300"
             >
               {item.name}
             </Link>
           ))}
         </ul>
       </nav>
-      <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-left duration-500" />
+      <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-left duration-200" />
       <Particles
-        className="absolute inset-0 -z-10 animate-fade-in duration-500"
+        className="absolute inset-0 -z-10 animate-fade-in duration-200"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-white duration-500 cursor-default font-display sm:text-6xl md:text-9xl whitespace-nowrap">
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-white duration-200 cursor-default font-display sm:text-6xl md:text-9xl whitespace-nowrap">
         Grace Wang
       </h1>
 
-      <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-right duration-500" />
-      <div className="my-16 text-center animate-fade-in duration-500">
+      <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-right duration-200" />
+      <div className="my-16 text-center animate-fade-in duration-200">
         <h2 className="text-lg md:text-xl text-gray-400">
           Second Year Business and Computer Science
         </h2>
