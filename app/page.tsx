739c21ae7154bc2162c832f4f59eeed147e41d83
Grace <wanggrace730@gmail.com>
2024-09-17 12:15:22 -0700

    changed colours

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-pink-100">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-pink-100 via-pink-200 to-pink-300">
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-gray-800 hover:text-gray-600"
+              className="text-sm duration-500 text-gray-400 hover:text-gray-300"
             >
               {item.name}
             </Link>
@@ -28,13 +28,13 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-white duration-300 cursor-default font-display sm:text-6xl md:text-9xl whitespace-nowrap">
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-gray-200 duration-300 cursor-default font-display sm:text-6xl md:text-9xl whitespace-nowrap">
         Grace Wang
       </h1>
 
       <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-right" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-gray-1000">
+        <h2 className="text-sm text-gray-400">
           Second Year Business and Computer Science Student
         </h2>
       </div>
