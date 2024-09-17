6d4b175ceceef962f41c68108b15f018ebb6ab27
Grace <wanggrace730@gmail.com>
2024-09-17 13:18:34 -0700

    shortened fade in

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-pink-100 via-pink-200 to-pink-300">
-      <nav className="my-16 animate-fade-in">
+      <nav className="my-16 animate-fade-in duration-500">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
@@ -23,17 +23,17 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-      <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-left" />
+      <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-left duration-500" />
       <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
+        className="absolute inset-0 -z-10 animate-fade-in duration-500"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-white duration-100 cursor-default font-display sm:text-6xl md:text-9xl whitespace-nowrap">
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-white duration-500 cursor-default font-display sm:text-6xl md:text-9xl whitespace-nowrap">
         Grace Wang
       </h1>
 
-      <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-right" />
-      <div className="my-16 text-center animate-fade-in">
+      <div className="hidden w-screen h-px bg-gradient-to-r from-pink-100/0 via-gray-50/50 to-pink-100/0 md:block animate-fade-right duration-500" />
+      <div className="my-16 text-center animate-fade-in duration-500">
         <h2 className="text-lg md:text-xl text-gray-400">
           Second Year Business and Computer Science Student
         </h2>
